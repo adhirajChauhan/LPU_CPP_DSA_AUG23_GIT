9db393c0db5124c8217086d7997d67223239fe40
adhirajChauhan <adhirajsingh0105@gmail.com>
2023-08-02 19:36:46 +0530

    First commit

---
+++ CPP/01_Introduction/hello.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //Printing Hello world
+    cout << "Hello World" << endl;
+}
---
+++ CPP/02_DataTypes_Variables/01_intro.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // const int myInteger = 5;
+    // // myInteger = 10;
+    // double myDouble = 5.5;
+    // float myFloat = 10.5;
+    // char myChar = 'a';
+    // bool myBool = true;
+    // string myString = "Hello";
+
+    // cout << myInteger << endl;
+    // cout << myDouble << endl;
+    // cout << myFloat << endl;
+    // cout << myChar << endl;
+    // cout << myBool << endl;
+    // cout << myString << endl;
+
+    // int userInput;
+    string name;
+
+    cout << "Enter your name : ";
+
+    cin >> name;
+
+    cout << "Number is : " << name << endl;
+
+}
+
+
+//Take user input for 2 numbers
+//Print the Value of below arithmetic operators
+
+//Add +
+//Sub -
+//Mul *
+// Div /
+// Modulus %
