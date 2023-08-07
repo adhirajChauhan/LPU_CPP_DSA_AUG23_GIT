5b437c8a40e2037f192c5ff1d81d56b6c5eb0f5b
adhirajChauhan <adhirajsingh0105@gmail.com>
2023-08-07 20:51:14 +0530

    07082023

---
+++ CPP/05_Functions/02_function_parameters.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+// void printName(string name = "Hello"){
+//     cout << "The name is : " << name << endl;
+// } 
+
+void fullName(string firstName, string lastName){
+    cout << "First name is : " << firstName << " and last name is : " << lastName;
+}
+
+int add(int num1, int num2){
+    return num1 + num2;
+}
+
+//name, 12th marks
+//My name is < > and i got < > in 12th
+
+int main(){
+    // printName();
+    // printName("World");
+
+    // fullName("Hello", "World");
+
+    cout << add(5,5);
+    
+}
+
+
+//Define a function that returns the product of two numbers entered by user.
+
+
+// Define two functions to print the maximum and the minimum number respectively among three numbers entered by user.
