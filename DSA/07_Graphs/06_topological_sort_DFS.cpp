71f0ea751e4ad0db6f3f619fbe9cb5f762f2fccb
adhirajChauhan <adhirajsingh0105@gmail.com>
2023-11-04 19:30:19 +0530

    04112023

---
+++ DSA/07_Graphs/06_topological_sort_DFS.cpp
@@ -0,0 +1,43 @@
+// #include <bits/stdc++.h> 
+
+// void topSort(int node, vector<bool> &visited, stack<int> &s, unordered_map<int, list<int>> &adj){
+//     visited[node] = 1;
+
+//     for(auto neighbours : adj[node]){
+//         if(!visited[neighbours]){
+//             topSort(neighbours, visited, s, adj);
+//         }
+//     }
+
+//     s.push(node);
+// }
+
+// vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
+//     unordered_map<int, list<int>> adj;
+
+//     for(int i = 0; i< e; i++){
+//         int u = edges[i][0];
+//         int v = edges[i][1];
+
+//         adj[u].push_back(v);
+
+//         vector<bool> visited(v);
+
+//         stack<int> s;
+
+//         for(int i = 0; i < v; i++){
+//             if(!visited[i]){
+//                 topSort(i, visited, s, adj);
+//             }
+//         }
+
+//         vector<int> ans;
+//         while(!s.empty()){
+//             ans.push_back(s.top());
+//             s.pop();
+//         }
+
+//         return ans;
+
+//     }
+// }
